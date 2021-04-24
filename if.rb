e6bbb035ce601bb286ac6619bf9e8fe37ad72d9d
EC2 Default User <korethis0816@gmail.com>
2021-04-24 04:48:48 +0000

    [Start]ruby

---
+++ if.rb
@@ -0,0 +1,20 @@
+n = 11
+if n > 10
+  puts '10より大きい'
+else
+  puts '10以下'
+end
+
+
+country = 'italy'
+if country == 'japan'
+  puts 'こんにちは'
+elsif country == 'us'
+  puts 'hello'
+elsif country == 'italy'
+  puts 'ciao'
+else
+  puts '???'
+end
+
+
---
+++ point.rb
@@ -0,0 +1,3 @@
+point = 7
+day = 1
+puts point *= 5 if day == 1
