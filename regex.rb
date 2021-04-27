c17f09bbe733e003b2549f45e761d9b2d3814f94
EC2 Default User <korethis0816@gmail.com>
2021-04-27 03:42:06 +0000

    [Update]ruby

--- regex.rb
+++ regex.rb
@@ -45,3 +45,10 @@ puts text.gsub(/,|-/, ':')
 
 text = '誕生日は1977年7月17日です。'
 puts text.gsub(/(\d+)年(\d+)月(\d+)日/, '\1-\2-\3')
+
+text = '誕生日は1977年7月17日です。'
+puts text.gsub(/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/, '\k<year>-\k<month>-\k<day>')
+
+text = '123,456-789'
+hash = {',' => ':', '-' => '/'}
+puts text.gsub(/,|-/, hash)
