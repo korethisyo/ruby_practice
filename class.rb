33089887fea64c9736c2dd284dd08ceb58f1403c
EC2 Default User <korethis0816@gmail.com>
2021-07-05 03:41:20 +0000

    self

--- class.rb
+++ class.rb
@@ -53,3 +53,28 @@ end
 
 user = User.new
 user.hello
+
+class User
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def hello
+    "Hello, I am #{name}"
+  end
+
+  def hi
+    "Hi, I am #{self.name}"
+  end
+
+  def my_name
+    "My name is #{@name}"
+  end
+end
+
+user = User.new('Alice')
+puts user.hello
+puts user.hi
+puts user.my_name
