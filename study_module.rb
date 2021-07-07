dc4817b0d18cfd7e28167b187146f2dc146d8f7e
EC2 Default User <korethis0816@gmail.com>
2021-07-07 07:07:50 +0000

    モジュール

---
+++ study_module.rb
@@ -0,0 +1,45 @@
+module Loggable
+  private
+
+  def log(text)
+    puts "[LOG] #{text}"
+  end
+end
+
+class Product
+  include Loggable
+
+  def title
+    log 'title is called.'
+    'A great movie'
+  end
+  
+  # 下記logメソッドをProduct＆Userクラスで使用。1行目からのModuleでlogメソッドを定義。
+  # private
+
+  # def log(text)
+  #   puts "[LOG] #{text}"
+  # end
+end
+
+class User
+  include Loggable
+
+  def name
+    log 'name is called.'
+    'Alice'
+  end
+
+  # private
+
+  # def log(text)
+  #   puts "[LOG] #{text}"
+  # end
+end
+
+
+product = Product.new
+product.title
+
+user = User.new
+user.name
