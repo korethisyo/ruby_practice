b61ff5e7042084fb4e4c192025ee4cffde7ad2b3
EC2 Default User <korethis0816@gmail.com>
2021-07-08 06:18:19 +0000

    名前空間

--- study_module.rb
+++ study_module.rb
@@ -43,3 +43,29 @@ product.title
 
 user = User.new
 user.name
+
+
+
+# 名前空間
+# クラス名が重複する時、moduleの中でクラス定義をすると区別ができる
+module Baseball
+  class Second
+    def initialize(player, uniform_number)
+      @player = player
+      @uniform_number = uniform_number
+    end
+  end
+end
+
+module Clock
+  class Second
+    def initialize(digits)
+      @digits = digits
+    end
+  end
+end
+
+# モジュールに属するクラスを参照する
+Baseball::Second.new('Shohei', 17)
+
+Clock::Second.new(17)
