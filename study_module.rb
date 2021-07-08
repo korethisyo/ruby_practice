83b7b0dda2da9721c9055614184b03e63e426b73
EC2 Default User <korethis0816@gmail.com>
2021-07-08 06:50:33 +0000

    [Update]Module

--- study_module.rb
+++ study_module.rb
@@ -69,3 +69,50 @@ end
 Baseball::Second.new('Shohei', 17)
 
 Clock::Second.new(17)
+
+
+
+# 関数や定数を提供するモジュールの作成
+# モジュール単体でメソッドの呼び出し
+module Accessable
+  # 特異メソッドで定義
+  # def self.log(text)
+    # "[Log] #{text}"
+  # end
+
+  # 下記の書き方でも可能
+  class << self
+    def log(text)
+      "[Log] #{text}"
+    end
+  end
+end
+
+puts Accessable.log('Hi')
+
+
+
+# module_functionメソッド
+module Disposable
+  def log(text)
+    puts "[Log] #{text}"
+  end
+
+  module_function :log
+end
+
+# module_functionメソッドでlogを特異メソッドとして呼び出す
+Disposable.log('white')
+
+class Mask
+  include Disposable
+
+  def kind
+    # includeしたモジュールのlogメソッドの呼び出し
+    log 'kind is called.'
+    'hana-selebu'
+  end
+end
+
+mask = Mask.new
+mask.kind
