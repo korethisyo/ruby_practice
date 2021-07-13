651aca1b84f8cd7ffa71519a5a93f218f5a3e87b
EC2 Default User <korethis0816@gmail.com>
2021-07-13 04:14:28 +0000

    デバッグ

--- test/test_fizz_buzz.rb
+++ test/test_fizz_buzz.rb
@@ -3,6 +3,8 @@ require '../lib/fizz_buzz'
 
 class FizzBuzzTest < Minitest::Test
   def test_fizz_buzz
+    # デバッグbyebugの使用
+    # require 'byebug'; byebug
     assert_equal '1', fizz_buzz(1)
     assert_equal '2', fizz_buzz(2)
     assert_equal 'Fizz', fizz_buzz(3)
