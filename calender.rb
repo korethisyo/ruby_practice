de8885830f8f8f35aa051a26e67b8fc55d969375
EC2 Default User <korethis0816@gmail.com>
2021-08-07 05:22:23 +0000

    カレンダー作成

--- calender.rb
+++ calender.rb
@@ -15,8 +15,24 @@ def calendar
   # 月の最初の週の日曜日を抜き出す
   start_day = first_date - first_date.strftime('%w').to_i
 
+  # strftime (%B 月の名称(January, February...) %Y 西暦)
   puts first_date.strftime('%B %Y')
   puts 'Su Mo Tu We Th Fr Sa'
+
+  while start_day.month <= first_date.month
+
+    if start_day.month != first_date.month
+      print "\s\s\s"
+    # %u を使い、土曜日を"6"として、土曜日の時、改行する
+    elsif start_day.strftime('%u') == "6"
+      print "\s" + start_day.strftime('%e') + "\n"
+    else
+      print "\s" + start_day.strftime('%e')
+    end
+
+    start_day += 1
+  end
+
 end
 
 puts calendar
