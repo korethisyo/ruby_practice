e277159067321ffc5504628d8422d4029750f414
EC2 Default User <korethis0816@gmail.com>
2021-08-08 03:50:19 +0000

    [Update]KaraokeMachine

--- karaoke.rb
+++ karaoke.rb
@@ -2,4 +2,9 @@ class KaraokeMachine
   def initialize(melody)
     @melody = melody
   end
+
+  def transpose(num)
+    # 音楽のコード”ドレミファソ”の英語表記 => "CDEFGAB" => 半音上がるのが"C#, D#, F#, G#, A#"の5音
+    codes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+  end
 end
