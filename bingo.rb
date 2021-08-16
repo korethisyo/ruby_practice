d0a297cabbc9d1babb3d832f14bbb07771baf350
EC2 Default User <korethis0816@gmail.com>
2021-08-16 04:13:41 +0000

    [Update]bingo

--- bingo.rb
+++ bingo.rb
@@ -1,8 +1,20 @@
 class Bingo
   def self.generate_card
     title = "BINGO".split(//).map do |bingo|
+      # 1行目のBINGOを右詰め
       sprintf("%3s", bingo)
+      # 各文字の右側に"｜"を出力 => bingo + " | "で記述していたときは、"O"の後にも"|"が出力されていた。joinの場合、文字を区切る箇所のみで"|"
     end.join(" | ")
+
+    # 75の数を持つ配列をさらに15個ずつの配列に分ける
+    numbers = (1..75).each_slice(15).map do |n|
+      n.sample(5)
+    end
+    
+    # numbers配列の2個目の配列の2番目の数値の箇所を空白にし、ビンゴカードの真ん中にする
+    numbers[2][2] = " "
+    
+    
   end
 end
 
