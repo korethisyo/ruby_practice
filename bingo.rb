513f4dbfd6cfc2de6c19f3f7df5cff9f44c58b5a
EC2 Default User <korethis0816@gmail.com>
2021-08-17 03:12:04 +0000

    [Update]bingo

--- bingo.rb
+++ bingo.rb
@@ -15,15 +15,16 @@ class Bingo
     # numbers配列の2個目の配列の2番目の数値の箇所を空白にし、ビンゴカードの真ん中にする
     numbers[2][2] = " "
 
-    numbers.transpose.join(" | ")
-
     # transposeメソッドで配列を行・列とみなし入れ替える
-    # numbers.transpose.map do |number|
-    #   number.map do |n|
-    #     # 数値を右詰めで表示
-    #     sprintf("%3d", n)
-    #   end
-    # end.join(" | ")
+    new_numbers = numbers.transpose.map do |number|
+      # numbers配列は10行目の処理で、配列の中にさらに配列ができた。
+      # 19行目のmapメソッドでは配列の中の配列を取り出したので、配列の中の数値を参照したいからさらにmapを使用する。
+      number.map do |n|
+        # 数値を右詰めで表示
+        n.to_s.rjust(3)
+      end.join(" | ")
+    # 配列の中の配列は５つの数値を持っているので、配列１つで区切れば自然と5個目の数値で改行できる
+    end.join("\n")
   end
 end
 
