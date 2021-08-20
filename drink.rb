200b2e2616bb935b7b839324d576fd1749a8f211
EC2 Default User <korethis0816@gmail.com>
2021-08-20 04:01:05 +0000

    [Fin]BonusDrink

--- drink.rb
+++ drink.rb
@@ -1,9 +1,12 @@
+# 問題文
+# ある駄菓子屋で飲み物を買うと、空き瓶3本で新しい飲み物を1本プレゼントしてくれる。最初に100本購入した場合、トータルで何本飲めるか
+
 class BonusDrink
   def self.for_total(num)
     if num % 2 == 0
       num + (num - 2) / 2
     else num % 2 != 0
-      "Hi"
+      num + (num - 1) / 2
     end
   end
 end
