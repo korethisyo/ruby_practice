db834d388e27aec6f7c7d04ff35a76c84e6401c7
EC2 Default User <korethis0816@gmail.com>
2021-08-23 03:51:22 +0000

    [Update]NameIndex

--- name_index.rb
+++ name_index.rb
@@ -2,11 +2,20 @@
 # 各要素は50音順にソートもすること。
 
 class NameIndex
+  # 50音で分別するときにア~ンの文字が必要
+  KANAMOJI = ('ア'..'ン').to_a
+  # ア段でグループ分けの際に必要
+  A_SOUNDS = ["ア", "カ", "サ", "タ", "ナ", "ハ", "マ", "ヤ", "ラ", "ワ"]
+
   # 配列の要素の個数は不特定なので、＊を引数につけて配列に
   def self.create_index(*names)
-    names.map{
+    array = names.map {
      |name| [] << name
     }
+
+    # array.map {
+    #   |a| a.match(KANAMOJI)
+    # }
   end
 end
 
